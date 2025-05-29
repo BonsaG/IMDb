caa6004270bfaaf3a61dbb1d15e051507b27223d
bonsa girma <bonsagirma1995@gmail.com>
2025-05-29 15:05:10 +0300

    create the about page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+// import About from "./about/Page";
 // import Navbar from "../components/Navbar";
 // import SearchBox from "../components/SearchBox";
 
@@ -7,15 +8,29 @@ import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function() {
+              const theme = localStorage.theme;
+              if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+              } else {
+                document.documentElement.classList.remove('dark');
+              }
+            })();
+          `,
+          }}
+        />
+      </head>
       <body>
         <Providers>
           {/* Header */}
           <Header />
 
-          {/* Navbar */}
-
-          {/* <Navbar /> */}
+          {/* <about /> */}
 
           {/* SearchBox */}
 
