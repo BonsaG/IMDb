3704e0a5fd5c3c8706bdb75053105960f5a1b988
bonsa girma <bonsagirma1995@gmail.com>
2025-05-30 12:34:56 +0300

    the last update

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function NavbarItem({ title, param }) {
@@ -9,18 +8,16 @@ export default function NavbarItem({ title, param }) {
   const genre = searchParams.get("genre");
   return (
     <div>
-      <Suspense>
-        <Link
-          className={`m-4 hover:text-amber-600 font-semibold p-2 ${
-            genre &&
-            genre === param &&
-            "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"
-          }`}
-          href={`/?genre=${param}`}
-        >
-          {title}
-        </Link>
-      </Suspense>
+      <Link
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
   );
 }
